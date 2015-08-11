e210fdc9942e16645396f90174d4adb575b15b28
gshaik <gshaik@semprautilities.com>
2015-08-11 10:28:12 -0700

    phase names added

--- ViewController/src/PLBean2.java
+++ ViewController/src/PLBean2.java
@@ -1,3 +1,5 @@
+import javax.faces.event.ActionEvent;
+
 public class PLBean2 {
     public PLBean2() {
     }
@@ -7,5 +9,16 @@ public class PLBean2 {
         int i =1/0;
         System.out.println("2222222222222222222222");
         return null;
+    }
+
+    public String actionMethod() {
+        // Add event code here...
+        System.out.println("action");
+        return "";
+    }
+
+    public void mehtod1ActionListner(ActionEvent actionEvent) {
+        // Add event code here...
+        System.out.println("actionListner");
     }
 }
--- ViewController/src/view/PageLoadBaseBean.java
+++ ViewController/src/view/PageLoadBaseBean.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 //import oracle.adf.controller.faces.context.FacesPageLifecycleContext;
+import oracle.adf.controller.faces.lifecycle.JSFLifecycle;
 import oracle.adf.controller.v2.lifecycle.Lifecycle;
 import oracle.adf.controller.v2.lifecycle.PagePhaseEvent;
 import oracle.adf.controller.v2.lifecycle.PagePhaseListener;
@@ -19,6 +20,27 @@ public class PageLoadBaseBean implements PagePhaseListener {
 
     private BindingContainer bc = null;
 
+
+/**
+ * 
+Lifecycle INIT_CONTEXT_ID : 0
+Lifecycle PREPARE_MODEL_ID : 1
+Lifecycle APPLY_INPUT_VALUES_ID : 2
+Lifecycle VALIDATE_INPUT_VALUES_ID : 3
+Lifecycle PROCESS_UPDATE_MODEL_ID : 4
+Lifecycle VALIDATE_MODEL_UPDATES_ID : 5
+Lifecycle PROCESS_COMPONENT_EVENTS_ID : 6
+Lifecycle METADATA_COMMIT_ID : 7
+Lifecycle PREPARE_RENDER_ID : 8
+    
+PhaseId RESTORE_VIEW : RESTORE_VIEW 1
+PhaseId APPLY_REQUEST_VALUES : APPLY_REQUEST_VALUES 2
+PhaseId PROCESS_VALIDATIONS : PROCESS_VALIDATIONS 3
+PhaseId UPDATE_MODEL_VALUES : UPDATE_MODEL_VALUES 4
+PhaseId INVOKE_APPLICATION : INVOKE_APPLICATION 5
+PhaseId RENDER_RESPONSE : RENDER_RESPONSE 6
+ * 
+ */
     /**
      * Before the ADF page lifecycle's prepareModel phase, invoke a
      * custom onPageLoad() method. Subclasses override the onPageLoad()
@@ -27,29 +49,30 @@ public class PageLoadBaseBean implements PagePhaseListener {
      */
     public void beforePhase(PagePhaseEvent pagePhaseEvent) {
      
-        System.out.println("Lifecycle INIT_CONTEXT_ID : " +Lifecycle.INIT_CONTEXT_ID);
-        System.out.println("Lifecycle PREPARE_MODEL_ID : " +Lifecycle.PREPARE_MODEL_ID);
-        System.out.println("Lifecycle APPLY_INPUT_VALUES_ID : " +Lifecycle.APPLY_INPUT_VALUES_ID);
-        System.out.println("Lifecycle VALIDATE_INPUT_VALUES_ID : " +Lifecycle.VALIDATE_INPUT_VALUES_ID);
-        System.out.println("Lifecycle PROCESS_UPDATE_MODEL_ID : " +Lifecycle.PROCESS_UPDATE_MODEL_ID);
-        System.out.println("Lifecycle VALIDATE_MODEL_UPDATES_ID : " +Lifecycle.VALIDATE_MODEL_UPDATES_ID);
-        System.out.println("Lifecycle PROCESS_COMPONENT_EVENTS_ID : " +Lifecycle.PROCESS_COMPONENT_EVENTS_ID);
-        System.out.println("Lifecycle METADATA_COMMIT_ID : " +Lifecycle.METADATA_COMMIT_ID);
-        System.out.println("Lifecycle PREPARE_RENDER_ID : " +Lifecycle.PREPARE_RENDER_ID);
-        System.out.println("    ");
-        System.out.println("PhaseId ANY_PHASE : " +PhaseId.ANY_PHASE);
-        System.out.println("PhaseId RESTORE_VIEW : " +PhaseId.RESTORE_VIEW);
-        System.out.println("PhaseId APPLY_REQUEST_VALUES : " +PhaseId.APPLY_REQUEST_VALUES);
-        System.out.println("PhaseId PROCESS_VALIDATIONS : " +PhaseId.PROCESS_VALIDATIONS);
-        System.out.println("PhaseId UPDATE_MODEL_VALUES : " +PhaseId.UPDATE_MODEL_VALUES);
-        System.out.println("PhaseId INVOKE_APPLICATION : " +PhaseId.INVOKE_APPLICATION);
-        System.out.println("PhaseId RENDER_RESPONSE : " +PhaseId.RENDER_RESPONSE);
-        
-        System.out.println("PhaseId VALUES : " +PhaseId.VALUES);
+//        System.out.println("Lifecycle INIT_CONTEXT_ID : " +Lifecycle.INIT_CONTEXT_ID);
+//        System.out.println("Lifecycle PREPARE_MODEL_ID : " +Lifecycle.PREPARE_MODEL_ID);
+//        System.out.println("Lifecycle APPLY_INPUT_VALUES_ID : " +Lifecycle.APPLY_INPUT_VALUES_ID);
+//        System.out.println("Lifecycle VALIDATE_INPUT_VALUES_ID : " +Lifecycle.VALIDATE_INPUT_VALUES_ID);
+//        System.out.println("Lifecycle PROCESS_UPDATE_MODEL_ID : " +Lifecycle.PROCESS_UPDATE_MODEL_ID);
+//        System.out.println("Lifecycle VALIDATE_MODEL_UPDATES_ID : " +Lifecycle.VALIDATE_MODEL_UPDATES_ID);
+//        System.out.println("Lifecycle PROCESS_COMPONENT_EVENTS_ID : " +Lifecycle.PROCESS_COMPONENT_EVENTS_ID);
+//        System.out.println("Lifecycle METADATA_COMMIT_ID : " +Lifecycle.METADATA_COMMIT_ID);
+//        System.out.println("Lifecycle PREPARE_RENDER_ID : " +Lifecycle.PREPARE_RENDER_ID);
+//        System.out.println("    ");
+//       // System.out.println("PhaseId ANY_PHASE : " +PhaseId.ANY_PHASE);
+//        System.out.println("PhaseId RESTORE_VIEW : " +PhaseId.RESTORE_VIEW);
+//        System.out.println("PhaseId APPLY_REQUEST_VALUES : " +PhaseId.APPLY_REQUEST_VALUES);
+//        System.out.println("PhaseId PROCESS_VALIDATIONS : " +PhaseId.PROCESS_VALIDATIONS);
+//        System.out.println("PhaseId UPDATE_MODEL_VALUES : " +PhaseId.UPDATE_MODEL_VALUES);
+//        System.out.println("PhaseId INVOKE_APPLICATION : " +PhaseId.INVOKE_APPLICATION);
+//        System.out.println("PhaseId RENDER_RESPONSE : " +PhaseId.RENDER_RESPONSE);
+//        
+//        //System.out.println("PhaseId VALUES : " +PhaseId.VALUES);
             
-        System.out.println("    ");
+      
         System.out.println("    ");
         System.out.println("------Before Phase id    : "+pagePhaseEvent.getPhaseId());
+        System.out.println(  "Before The Phase: " + this.getPhaseName(pagePhaseEvent.getPhaseId())  );
         //        FacesPageLifecycleContext ctx =
         //            (FacesPageLifecycleContext)event.getLifecycleContext();
         //        if (event.getPhaseId() == Lifecycle.PREPARE_MODEL_ID) {
@@ -61,6 +84,7 @@ public class PageLoadBaseBean implements PagePhaseListener {
 
     public void afterPhase(PagePhaseEvent pagePhaseEvent) {
     System.out.println("-----After Phase id    : "+pagePhaseEvent.getPhaseId());
+        System.out.println(  "After The Phase: " + this.getPhaseName(pagePhaseEvent.getPhaseId())  );
         if (pagePhaseEvent.getPhaseId() == Lifecycle.PREPARE_RENDER_ID) {
             FacesContext facesCtx = FacesContext.getCurrentInstance();
             ExternalContext extCtx = facesCtx.getExternalContext();
@@ -77,14 +101,18 @@ public class PageLoadBaseBean implements PagePhaseListener {
 
 
     public void onPageLoad() {
+        
+        
         // Subclasses can override this.
     }
 
     public void onPagePreRender() {
+      
         // Subclasses can override this.
     }
 
     protected boolean isPostback() {
+        
         return Boolean.TRUE.equals(ADFUtils.invokeEL("#{adfFacesContext.postback}"));
     }
     /*  protected EWarrantyAdminModule getEWarrantyAdminModule() {
@@ -99,4 +127,40 @@ public class PageLoadBaseBean implements PagePhaseListener {
         protected DCIteratorBinding getIteratorBinding(String name) {
           return ((DCBindingContainer)getBindingContainer()).findIteratorBinding(name);
         }*/
+    
+    
+    public String getPhaseName(int phaseId) {
+      if (phaseId == JSFLifecycle.INIT_CONTEXT_ID) {
+       return "INIT_CONTEXT";
+      } else if (phaseId == JSFLifecycle.PREPARE_MODEL_ID) {
+       return "PREPARE_MODEL";
+      } else if (phaseId == JSFLifecycle.APPLY_INPUT_VALUES_ID) {
+       return "APPLY_INPUT_VALUES";
+      } else if (phaseId == JSFLifecycle.VALIDATE_INPUT_VALUES_ID) {
+       return "VALIDATE_INPUT_VALUES";
+      } else if (phaseId == JSFLifecycle.PROCESS_UPDATE_MODEL_ID) {
+       return "PROCESS_UPDATE_MODEL";
+      } else if (phaseId == JSFLifecycle.VALIDATE_MODEL_UPDATES_ID) {
+       return "VALIDATE_MODEL_UPDATES";
+      } else if (phaseId == JSFLifecycle.PROCESS_COMPONENT_EVENTS_ID) {
+       return "PROCESS_COMPONENT_EVENTS";
+      } else if (phaseId == JSFLifecycle.METADATA_COMMIT_ID) {
+       return "METADATA_COMMIT";
+      } else if (phaseId == JSFLifecycle.PREPARE_RENDER_ID) {
+       return "PREPARE_RENDER";
+      } else if (phaseId == JSFLifecycle.JSF_RESTORE_VIEW_ID) {
+       return "RESTORE_VIEW";
+      } else if (phaseId == JSFLifecycle.JSF_APPLY_REQUEST_VALUES_ID) {
+       return "JSF_APPLY_REQUEST_VALUES";
+      } else if (phaseId == JSFLifecycle.JSF_PROCESS_VALIDATIONS_ID) {
+       return "JSF_PROCESS_VALIDATIONS";
+      } else if (phaseId == JSFLifecycle.JSF_UPDATE_MODEL_VALUES_ID) {
+       return "JSF_UPDATE_MODEL_VALUES";
+      } else if (phaseId == JSFLifecycle.JSF_INVOKE_APPLICATION_ID) {
+       return "JSF_INVOKE_APPLICATION";
+      } else {
+       return "JSF_RENDER_RESPONSE";
+      }
+    }
+     
 }
