8479bade0fd13e5ff247cc6d6e3c10e96f845780
gshaik <gshaik@semprautilities.com>
2016-06-08 17:46:39 -0700

    text added

---
+++ Model/src/model/ModelsExceptionHandler.java
@@ -0,0 +1,59 @@
+package model;
+
+import oracle.adf.model.BindingContext;
+import oracle.adf.model.binding.DCBindingContainer;
+import oracle.adf.model.binding.DCErrorHandlerImpl;
+
+public class ModelsExceptionHandler extends DCErrorHandlerImpl {
+    public ModelsExceptionHandler() {
+        super(true);
+    }
+
+    /**
+     * This method will report the exceptions to the caller
+     * (The caller can be a Taskflow activity or a managed bean method)
+     *
+     * @param dCBindingContainer
+     * @param exception
+     */
+    @Override
+    public void reportException(DCBindingContainer dCBindingContainer,
+                                Exception exception) {
+        System.out.println("Entering Model Exception Handler:reportException");
+        //ScgExceptionUtils.handleException(exception);
+        super.reportException(dCBindingContainer, exception);
+        System.out.println("Exiting Model Exception Handler:reportException");
+
+
+        //get message to be displayed from resource bundle or hardcode the message when throwing exception
+//        String msg = XxUtils.getMessageString((XxCustomException)ex);
+//        JboException jex = new JboException(msg);
+//        super.reportException(formBnd, jex);
+
+
+    }
+
+    /**
+     * This method will return the exception message displayed to the user
+     * We are suppressing the message by returning null since we want to handle
+     * the display of message to the end user in the Caller (i.e ManagedBean or Taskflow)
+     *
+     * @param bindingContext
+     * @param exception
+     * @return
+     */
+    @Override
+    public String getDisplayMessage(BindingContext bindingContext,
+                                    Exception exception) {
+        //return super.getDisplayMessage(bindingContext, exception);
+
+        // Suppressing the default behaviour of displaying exception
+        // message to the user.
+        return super.getDisplayMessage(bindingContext, exception);
+    }
+
+    @Override
+    protected boolean skipException(Exception exception) {
+        return super.skipException(exception);
+    }
+}
--- Readme.txt
+++ Readme.txt
@@ -1,6 +1,24 @@
-create a java class that extends 
-and register in below path
-\ADFLifecycle_exception\ViewController\adfmsrc\META-INF\adf-settings.xml
+How to configure an ADF Phase Listener
+MyAdfListener implements PagePhaseListener{ 
+In \src\META-INF\adf-settings.xml
+
+<?xml version="1.0" encoding="windows-1252" ?>
+<adf-settings xmlns="http://xmlns.oracle.com/adf/settings">
+<adfc-controller-config 
+xmlns="http://xmlns.oracle.com/adf/controller/config">
+<lifecycle>
+<phase-listener>
+<listener-id>MyAdfListener</listener-id>
+<class>adf.sample.MyAdfListener </class>
+</phase-listener>
+</lifecycle>
+</adfc-controller-config> 
+</adf-settings>
+
+
+
+
+
 
 for the controller level exception handling create a custom class that extends 
 oracle.adf.view.rich.context.ExceptionHandler and then override 
@@ -11,3 +29,12 @@ and add a text file in folder .adf\META-INF\services in application
 with name oracle.adf.view.rich.context.ExceptionHandler
 and put view.exception.XxControllerExpHandler in that file 
 C:\JDeveloper\mywork\ADFLifecycle_exception\.adf\META-INF\services
+
+
+ignore
+
+create a java class that extends  oracle.adf.view.rich.context.ExceptionHandler
+and register in below path
+\ADFLifecycle_exception\ViewController\adfmsrc\META-INF\adf-settings.xml
+
+ignore
